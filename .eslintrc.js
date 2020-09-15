b76c9b61f827a40acb0e112d9b070701c0d29321
daniandl <danian.deleeuw@gmail.com>
2020-09-16 01:05:31 +0200

    chore: add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,10 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'arrow-parens': ['error', 'as-needed'],
+    'import/prefer-default-export': 'off',
+    'no-restricted-syntax': ['error', 'ForOfLoop'],
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'no-continue': 'off',
+    'vars-on-top': 'off', // for-loop micro-optimization
   },
 }
