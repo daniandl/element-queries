c11cf558a16873a7b921ca0b329f294431f628ef
daniandl <danian.deleeuw@gmail.com>
2020-11-06 15:45:40 +0100

    chore: proper todo file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
 import ElementQueries from './ElementQueries'
 
 export default ElementQueries
-
-/** TODOs
- * TODO move this out of here :)
- * TODO allow passing breakpoint object to .watch() which overrides htmlAttrBreakpoints
- * TODO Support HTMLElement *AND* SVGElement
- * TODO disable if ResizeObserver isnt supported
- * NOTE general browser-support checks (MutationObserver, etc)
- * NOTE batching resize updates?
- * NOTE throttle observer?
- * NOTE debounce updates on a per-element basis?
- * NOTE use requestAnimationFrame on resize callback?
- * NOTE what do if user changes htmlAttrBreakpoints dynamically
- */
